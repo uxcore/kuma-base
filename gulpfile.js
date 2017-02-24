686f6af74f244f93a05cc4b9f4cceaf7165813c1
光弘 <guanghong.wsj@alibaba-inc.com>
2017-02-24 20:28:18 +0800

    add tem

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var connect = require('gulp-connect');
+var ejs = require('gulp-ejs');
 
 var LessPluginAutoPrefix = require('less-plugin-autoprefix');
-// var LessPluginGlob = require('less-plugin-glob');
 var LessPluginInlineUrls = require('less-plugin-inline-urls');
 var autoprefixPlugin = new LessPluginAutoPrefix({browsers: [
     '> 5%',
@@ -16,6 +16,57 @@ var lessDevConfig = {
         LessPluginInlineUrls
     ]
 };
+
+var themes = [
+    {
+        name: 'alipay',
+        brandPrimary: 'rgba(0, 164, 230, 1)',
+        brandPrimaryLightAlpha: 'rgba(0, 164, 230, 0.7)',
+    },
+    {
+        name: 'ant_financial',
+        brandPrimary: 'rgba(0, 160, 232, 1)',
+        brandPrimaryLightAlpha: 'rgba(0, 160, 232, 0.7)',
+    },
+    {
+        name: 'ants_daq',
+        brandPrimary: 'rgba(0, 160, 232, 1)',
+        brandPrimaryLightAlpha: 'rgba(0, 160, 232, 0.7)',
+    },
+    {
+        name: 'blue',
+        brandPrimary: 'rgba(37, 153, 242, 1)',
+        brandPrimaryLightAlpha: 'rgba(37, 153, 242, 0.7)',
+    },
+    {
+        name: 'koubei',
+        brandPrimary: 'rgba(226, 71, 14, 1)',
+        brandPrimaryLightAlpha: 'rgba(226, 71, 14, 0.7)',
+    },
+    {
+        name: 'mybank',
+        brandPrimary: 'rgba(0, 163, 171, 1)',
+        brandPrimaryLightAlpha: 'rgba(0, 163, 171, 0.7)',
+    },
+    {
+        name: 'orange',
+        brandPrimary: 'rgba(243, 115, 39, 1)',
+        brandPrimaryLightAlpha: 'rgba(243, 115, 39, 0.7)',
+    },
+    {
+        name: 'zhima_credit',
+        brandPrimary: 'rgba(46, 182, 169, 1)',
+        brandPrimaryLightAlpha: 'rgba(46, 182, 169, 0.7)',
+    },
+]
+
+gulp.task('makefiles', function() {
+    themes.forEach((theme) => {
+        gulp.src('./templates/theme.less')
+            .pipe(ejs(theme))
+            .pipe(gulp.dest(`./theme/${theme.name}.less`));
+    });
+});
 
 gulp.task('server', function(){
     connect.server({
